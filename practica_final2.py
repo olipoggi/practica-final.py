ad408ef4e1be276e00187f202139f8bcc13887b8
oliviadigital <olipoggisoqueff@gmail.com>
2024-12-16 12:33:37 -0300

    hicimos una practica de convertir medidas de centimetros a pulgadas

---
+++ practica_final2.py
@@ -0,0 +1,11 @@
+#1. solicitar al usuario las medidas de la pieza del mueble en cm
+
+medida_en_cms = float(input("Por favor, ingresar las medidas del mueble en centimentros: "))
+
+#2. convertir las medidas de centimentros a pulgadas
+
+medida_en_pulgadas = medida_en_cms /2.54
+
+#3. mostrar las medidas convertidas en pulgadas al usuario
+
+print ("las medidas en pulgadas de la pieza ingresada son:",  medida_en_pulgadas)
